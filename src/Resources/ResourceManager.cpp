1881117bc6a993a7c8f6be518ef8f7212dc4f9a4
Amirkhan <myrzabayev.amirkhan@gmail.com>
2025-02-11 16:52:48 +0100

    Bug fixes

--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -234,11 +234,11 @@ std::shared_ptr<RenderEngine::Texture2D> ResourceManager::loadTextureAtlas(const
 		unsigned int currentTextureOffsetY = textutreHeight;
 		for (const auto& currentSubTexureName : subTextures)
 		{
-			glm::vec2 leftBottomUV(static_cast<float>(currentTextureOffsetX) / textureWidth,
-									static_cast<float>(currentTextureOffsetY - subTextureHeight) / textutreHeight);
+			glm::vec2 leftBottomUV(static_cast<float>(currentTextureOffsetX + 0.01f) / textureWidth,
+									static_cast<float>(currentTextureOffsetY - subTextureHeight + 0.01f) / textutreHeight);
 
-			glm::vec2 rightTopUV(static_cast<float>(currentTextureOffsetX + subTextureWidth) / textureWidth,
-									static_cast<float>(currentTextureOffsetY) / textutreHeight);
+			glm::vec2 rightTopUV(static_cast<float>(currentTextureOffsetX + subTextureWidth - 0.01f) / textureWidth,
+									static_cast<float>(currentTextureOffsetY - 0.01f) / textutreHeight);
 
 			pTexture->addSubTexture(std::move(currentSubTexureName), leftBottomUV, rightTopUV);
 
