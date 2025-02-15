d6b57467fb259298fceb8a84faf554930abbc572
Amirkhan <myrzabayev.amirkhan@gmail.com>
2025-02-15 13:02:16 +0100

    Added explosion, fixed bugs

--- src/Game/GameObjects/Bullet.h
+++ src/Game/GameObjects/Bullet.h
@@ -2,6 +2,8 @@
 
 
 #include "IGameObject.h"
+#include "../../Renderer/SpriteAnimator.h"
+#include "../../System/Timer.h"
 
 #include <array>
 #include <memory>
@@ -26,20 +28,28 @@ public:
 	Bullet(const double velocity,
 	  	 const glm::vec2& position, 
 		 const glm::vec2& size,
+		const glm::vec2& explosionSize,
 		 const float layer);
 
 
 	virtual void render() const override;
+	void update(const double delta) override;
 	bool isActive() const { return m_isActive; }
 	void fire(const glm::vec2& position, glm::vec2& direction);
 	virtual void onCollision() override;
 
 private:
-	EOrientation m_eOrientation;
+	glm::vec2 m_explosionSize;
+	glm::vec2 m_explosionOffset;
 	std::shared_ptr<RenderEngine::Sprite> m_pSprite_top;
 	std::shared_ptr<RenderEngine::Sprite> m_pSprite_bottom;
 	std::shared_ptr<RenderEngine::Sprite> m_pSprite_left;
 	std::shared_ptr<RenderEngine::Sprite> m_pSprite_right;
+	std::shared_ptr<RenderEngine::Sprite> m_pSprite_explosion;
+	RenderEngine::SpriteAnimator m_spriteAnimator_explosion;
+	Timer m_explosionTimer;
+	EOrientation m_eOrientation;
 	double m_maxVeclocity;
 	bool m_isActive;
+	bool m_isExplosion;
 };
